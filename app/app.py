ae4a2e726957ada6d7464a1394f1b69f015b6d19
teilon <teiloncw@gmail.com>
2020-12-13 18:58:44 +0600

    OOP

--- app/app.py
+++ app/app.py
@@ -2,48 +2,64 @@ from collections import Counter
 from time import sleep
 from datetime import datetime
 from random import randrange
+from typing import List
 
 from selenium import webdriver
 
-target_url = "https://hh.kz/search/vacancy?area=160&fromSearchLine=true&st=searchVacancy&text=Python&from=suggest_post"
+CHROMEDRIVER_PATH = "../webdriver/chromedriver_86.0.4240.22/chromedriver"
+TARGET_URL = "https://hh.kz/search/vacancy?area=160&fromSearchLine=true&st=searchVacancy&text={}&from=suggest_post"
+MAX_ITERATION = 10
+MIN_TIMEOUT = 2
+MAX_TIMEOUT = 8
 
-def main():
-    chromedriver_path = "../webdriver/chromedriver_86.0.4240.22/chromedriver"
-    try:
-        driver = webdriver.Chrome(chromedriver_path)
-
-        driver.get(target_url)
-        # div[@class='vacancy-serp-item ']//
-        vacancies = driver.find_elements_by_xpath("//span[@class='g-user-content']//a")
+class HeadHunter():
+    
+    def __init__(self, search_word: str="Python", is_max_size: bool=False) -> None:
+        self.driver = webdriver.Chrome(CHROMEDRIVER_PATH)
+        self.search_word = search_word
+        self.is_max_size = is_max_size
+    
+    def run(self) -> None:
+        try:
+            urls = self.vacancies_list()
+            skills = self.skill_list(urls)
+        finally:
+            self.driver.quit()
+        
+        print(Counter(skills).most_common(6))
+    
+    def vacancies_list(self) -> List:
+        self.driver.get(TARGET_URL.format(self.search_word))
+        vacancies_elements = self.driver.find_elements_by_xpath("//span[@class='g-user-content']//a")
 
         urls = []
-        for i in vacancies:
+        for i in vacancies_elements:
             a = i.get_attribute('href')
             url = {
                 'href': a
             }
             urls.append(url)
-    
-        plates = []
-        count = 0
-        for j in urls:
-            sleep(randrange(2, 8))
-            print(datetime.now())
+        
+        return urls
 
-            driver.get(j['href'])
-            plate_tags = driver.find_elements_by_class_name("bloko-tag__section_text")
-            plate_elements = []
+    def skill_list(self, urls:List) -> List:
+        plates = []
+        _len = len(urls)
+        end = _len if self.is_max_size or MAX_ITERATION > _len else MAX_ITERATION
+        for url in urls[:end]:
+            self.pause()
+
+            self.driver.get(url['href'])
+            plate_tags = self.driver.find_elements_by_class_name("bloko-tag__section_text")
             for p in plate_tags:
                 plates.append(p.text.strip())
-            
-            count += 1
-            if count > 2:
-                break
-            
-        print(Counter(plates).most_common(10))
 
-    finally:
-        driver.quit()
+        return plates
+    
+    def pause(self):
+        sleep(randrange(MIN_TIMEOUT, MAX_TIMEOUT))
+        print(datetime.now())
 
 if __name__ == '__main__':
-    main()
+    parser = HeadHunter(is_max_size=True)
+    parser.run()
